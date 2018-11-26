0b1e0c5b8328925122be6121cb258498a95106ec
Joshua Alphonse <joshalphonse@gmail.com>
2018-11-26 18:10:57 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 def my_each(words) # put argument(s) here
   # code here
@@ -8,3 +9,14 @@ def my_each(words) # put argument(s) here
     i += 1
   end
   words
+=======
+def my_each (words)
+  i = 0
+while i > words.length
+
+yield(words[i])
+i += 1
+end
+words
+end
+>>>>>>> cb11e4e5703b90a6756a8778f331c09f643d87fb
